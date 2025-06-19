c80f7db18b00cccee82819129bdc64d84ffb3822
banhdeptraibodoithe <levevietnam@gmail.com>
2025-06-19 21:22:31 +0700

    Thêm đối tượng User

---
+++ QuanLySanPhamVoiOOP/User.js
@@ -0,0 +1,14 @@
+class User {
+    username;
+    password;
+    role;
+    email;
+    image;
+
+    constructor(username, password, email, role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
